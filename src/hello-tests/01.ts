b9ff46611c7510a48f4167978a05bd3b5dc76702
Denis <brokenhandq3@yandex.ru>
2022-11-20 20:12:40 +0300

    ts 01

---
+++ src/hello-tests/01.ts
@@ -0,0 +1,13 @@
+//in tsconfig set isolatedModules :false
+
+export  function sum(a: number, b: number) {
+    return a + b
+}
+
+export  function mult(a: number, b: number) {
+    return a * b
+}
+
+export function splitIntoWords(sentence :string) {
+    return sentence.replace('!', "").toLowerCase().split(' ')
+}
