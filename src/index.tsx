82dcbdaa87651ae6acacf77b06837d00265ccd7a
Denis <brokenhandq3@yandex.ru>
2022-11-20 20:10:09 +0300

    01.ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {splitIntoWords} from "./hello-tests/01";
+
+const sentence = 'hello my friend';
+const result = splitIntoWords(sentence)
+console.log(result[0] === 'hello', result[1] === 'my')
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
